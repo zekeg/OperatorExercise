afc07f84540dfe6cb440c96df15e2ff04e535c29
Zechariah Guadiana <zechariah.guadiana@gmail.com>
2022-03-03 18:30:20 -0600

    intial commit for operator exercise for true coders

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class OperatorExercise
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4) {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+        }
+    }
+}
